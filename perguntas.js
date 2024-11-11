f54911c7ab0eef228533aa6f928203c036fe301a
lohanaviana17 <Lohanaviana715@gmail.com>
2024-11-11 20:50:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+
+criaCartao(
+    'Geral',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas.'
+)
+
+criaCartao(
+    'Inglês',
+    'O que a palavra legend significa em português?',
+    'Lenda.'
+)
+
+criaCartao(
+    'Geral',
+    ' Quanto tempo a luz do Sol demora para chegar à Terra?',
+    ' 8 minutos.'
+)
